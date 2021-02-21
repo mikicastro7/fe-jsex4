bc3ae86fb620d66c634543f197ab61ff7622bf07
Miquel Castro <castro-miquel@hotmail.com>
2021-02-21 17:42:48 +0100

    Elimino los ;

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let palabras = ['café', 'croissant', 'tostada', 'zumo', "hola", "crack"];
+let palabras = ['café', 'croissant', 'tostada', 'zumo', "hola", "crack"]
 
 function modificaPares(palabras, modificarPalabra){
-    let palabrasModificadas = [];
+    let palabrasModificadas = []
     palabras.forEach((palabra, i) => {
         if((i + 1) % 2 === 0){
-            let modificada = modificarPalabra(palabra);
+            let modificada = modificarPalabra(palabra)
             palabrasModificadas.push(modificada)
         } else {
-            palabrasModificadas.push(palabra);
+            palabrasModificadas.push(palabra)
         }
-    });
+    })
 
     return palabrasModificadas
 }
 
-let palabrasParesModificadas = modificaPares(palabras, elemento => elemento.charAt(0).toUpperCase() + elemento.slice(1)); 
-console.log(palabrasParesModificadas);
+let palabrasParesModificadas = modificaPares(palabras, elemento => elemento.charAt(0).toUpperCase() + elemento.slice(1))
+console.log(palabrasParesModificadas)
